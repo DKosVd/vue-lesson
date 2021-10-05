7f45357570ed5407e5a660b79f92d5e6661bc9e7
DKosVd <dkosyhiyan@gmail.com>
2021-10-05 20:17:21 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,122 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="content">
+        <div class="header">
+            <div class="header-logo">
+              <img width="60" height="60" src="./assets/Logo.png"/>
+            </div>
+        </div>
+      <div class="main">
+          <div class="option-avia">
+              <span class="option-avia-title">Количество пересадок</span>
+              <div class="options-avia">
+                <div class="option">
+                    <input id="all" value="all" type="checkbox" v-model="aviaTransfer"/>
+                    <label for="all">Все</label>
+                </div>
+                <div class="option">
+                    <input id="without" value="without" type="checkbox" v-model="aviaTransfer"/>
+                    <label for="without">Без пересадок</label>
+                </div>
+                 <div class="option">
+                    <input id="one-avia" value="one-avia" type="checkbox" v-model="aviaTransfer"/>
+                    <label for="one-avia">1 пересадка</label>
+                </div>
+                 <div class="option">
+                    <input id="two-avia" value="two-avia" type="checkbox" v-model="aviaTransfer"/>
+                    <label for="two-avia">2 пересадки</label>
+                </div>
+                 <div class="option">
+                    <input id="three-avia" value="three-avia" type="checkbox" v-model="aviaTransfer"/>
+                    <label for="three-avia">3 пересадки</label>
+                </div>
+              </div>
+          </div>
+          <div class="avia-flights">
+            <div class="avia-flights-types">
+              <div class="avia-flights-type" :class="{
+                  'avia-flights-type--active': type == 'cheapset'
+              }" @click="chooseType('cheapset')">Самый дешевый</div>
+              <div class="avia-flights-type" :class="{
+                  'avia-flights-type--active': type == 'quick'
+              }" 
+               @click="chooseType('quick')">Самый быстрый</div>
+              <div class="avia-flights-type" :class="{
+                  'avia-flights-type--active': type == 'normal'
+              }"
+               @click="chooseType('normal')">Оптимальный</div>
+            </div>
+            <avia-card/>
+            <div class="show-flights">
+              <span>ПОКАЗАТЬ ЕЩЕ 5 БИЛЕТОВ!</span>
+            </div>
+          </div>
+      </div>
+      </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AviaCard from './components/AviaCard.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { AviaCard },
+  data() {
+    return {
+      type: '',
+      aviaTransfer: []
+    }
+  },
+
+
+  created() {
+    // const VALID_KEYS = ['type', 'aviaTransfer'];
+    let params = new URLSearchParams(new URL(window.location).search);
+    let type = window.localStorage.getItem('type');
+    let avia = params.get('avia')?.split(',');
+    // VALID_KEYS.forEach(key => {
+    //   this[key] = params[key]
+    // })
+    this.aviaTransfer = avia?.length >= 1 ? avia.filter(e => e): [];
+    this.type = type ? type : 'cheapset';
+  },
+
+
+  computed: {
+    pageStateOptions() {
+      return {
+        type: this.type,
+        aviaTransfer: this.aviaTransfer
+      }
+    },
+  },
+
+
+  watch: {
+    aviaTransfer() {
+      console.log(this.pageStateOptions.aviaTransfer.length)
+      if(this.pageStateOptions.aviaTransfer.length > 0) {
+        window.history.pushState(null, document.title, `${window.location.pathname}?avia=${this.pageStateOptions.aviaTransfer}`)
+        return
+      }
+      if(this.pageStateOptions.type) {
+        window.localStorage.setItem('type', this.pageStateOptions.type)
+      }
+      window.history.pushState(null, document.title, `${window.location.pathname}`)
+    },
+  },
+
+  methods: {
+    chooseType(type) {
+      this.type = type;
+      this.aviaTransfer = []
+    },
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="./style.css">
+
 </style>
---
+++ src/components/AviaCard.vue
@@ -0,0 +1,52 @@
+<template>
+                <div class="avia-flight">
+              <div class="avia-flight-container">
+                <div class="avia-flight-header">
+                  <div class="avia-flight-price">13 400 Р</div>
+                  <div class="avia-flight-logo">
+                    <img src="../assets/S7.png" width="110" height="36"/>
+                  </div>
+                </div>
+                <div class="avia-flight-main">
+                  <div class="avia-flight-params">
+                    <div class="avia-flight-param mb-24">
+                      <span class="avia-flight-param-text-gray">MOW - HKT</span>
+                      <span class="avia-flight-param-text">10:45 - 08:00</span>
+                    </div>
+                    <div class="avia-flight-param">
+                      <span class="avia-flight-param-text-gray">MOW - HKT</span>
+                      <span class="avia-flight-param-text">10:45 - 08:00</span>
+                    </div>
+                  </div>
+                    <div class="avia-flight-params">
+                    <div class="avia-flight-param mb-24">
+                      <span class="avia-flight-param-text-gray">В пути</span>
+                      <span class="avia-flight-param-text">21ч 15м</span>
+                    </div>
+                    <div class="avia-flight-param">
+                      <span class="avia-flight-param-text-gray">В пути</span>
+                      <span class="avia-flight-param-text">13ч 30м</span>
+                    </div>
+                  </div>
+                    <div class="avia-flight-params">
+                    <div class="avia-flight-param mb-24">
+                      <span class="avia-flight-param-text-gray">2 пересадки</span>
+                      <span class="avia-flight-param-text">HKG, JNB</span>
+                    </div>
+                    <div class="avia-flight-param">
+                      <span class="avia-flight-param-text-gray">1 пересадка</span>
+                      <span class="avia-flight-param-text">HKG</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+</template>
+
+<script>
+
+export default {
+    name: 'AviaCard'
+}
+
+</script>
